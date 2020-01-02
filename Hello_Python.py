36b95764dc22434a53858eeb36dc65c6b99f61bc
Harijeyan <jeyanhari@gmail.com>
2020-01-02 21:10:55 +0530

    Hello Python from Kaggle

---
+++ Hello_Python.py
@@ -0,0 +1,11 @@
+pizza_amount = 0
+print(pizza_amount)
+
+# ordering Pizza, Omlette, Pizza, Pizza, and Steak (4 more servings of Pizza)
+pizza_amount = pizza_amount + 4
+
+if pizza_amount > 0:
+    print("But I don't  need any Pizza")
+
+hut_song = "Pizza " * pizza_amount
+print(hut_song)
